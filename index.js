d7562bf81412fb6146b96f2498f449226edcfbb6
Ricardo Hernandez <rhernande@Macintosh-456.local>
2016-07-19 15:18:10 -0700

    Primary logic for marshalling

--- index.js
+++ index.js
@@ -2,15 +2,61 @@
 'use strict';
 var _ = require('lodash');
 var data = require('./data_helper')();
+var jsonfile = require('jsonfile');
 
-function transformHelper() {
-    var transformedData = {};
-    if (!_.isEmpty(data)) {
-        _.forEach(data, function(provider, key) {
+var properties = [
+    'establecimiento',
+    'rubro',
+    'servicio',
+    'direccion',
+    'colonia',
+    'municipio',
+    'cp',
+    'estado',
+    'telefono',
+    'descuento'
+];
 
+function app() {
+
+    var marshall = function() {
+        var transformedData = [];
+
+        if (!_.isEmpty(data)) {
+            console.log('Data is not empty');
+            _.forEach(data, function(provider, key) {
+                if (_.has(provider, '_aData')) {
+                    var arrayData = provider._aData;
+                    var providerObject = {};
+
+                    _.forEach(arrayData, function(propertyValue, key) {
+                        var property = properties[key];
+                        providerObject[property] = propertyValue;
+                    });
+
+                    transformedData.push(providerObject);
+                }
+            });
+        }
+        console.log(transformedData.length);
+        // writeJsonFile(transformedData);
+        return transformedData;
+    };
+
+    var writeJsonFile = function(data) {
+        var file = './data.json';
+
+        jsonfile.writeFile(file, data, {spaces: 4}, function(err) {
+            console.error(err);
         });
-    }
-    return transformedData;
+    };
+
+
+
+    return {
+        marshall: marshall,
+        writeJsonFile: writeJsonFile
+    };
 }
 
-module.exports = transformHelper;
+module.exports = app;
--- test/test_helper.js
+++ test/test_helper.js
@@ -6,27 +6,28 @@ var chai = require('chai');
 var _ = require('lodash');
 var expect = chai.expect;
 var assert = chai.assert;
-// var logicHelper = require('../logicHelper')();
-// var errorMessages = require('../errorMessages')();
 chai.config.includeStack = true;
-var data = require('../index')();
+var app = require('../index')();
 
 describe('Inapam Discounts', function() {
+    var data = app.marshall();
+
     it('Should contain data for providers', function() {
         expect(data).to.not.be.empty;
     });
+
     it('Should correct structure for provider', function() {
         var properties = [
-                'establecimiento',
-                'rubro',
-                'servicio',
-                'direccion',
-                'colonia',
-                'municipio',
-                'cp',
-                'estado',
-                'telefono',
-                'descuento'
+            'establecimiento',
+            'rubro',
+            'servicio',
+            'direccion',
+            'colonia',
+            'municipio',
+            'cp',
+            'estado',
+            'telefono',
+            'descuento'
         ];
 
         _.forEach(properties, function(property, key) {
